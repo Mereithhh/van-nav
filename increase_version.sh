d5dc3a91841aad2034104540165df3e2db0f6df3
mereith <wanglu@mereith.com>
2024-12-07 19:52:17 +0800

    chore: remove files

--- increase_version.sh
+++
@@ -1,6 +0,0 @@
-VERSIONNUM=$(cat ./version.txt)
-NEWSUBVERSION=`echo $VERSIONNUM | awk -F "." '{print $3}'`
-NEWSUBVERSION=$(($NEWSUBVERSION + 1))
-NEWVERSIONSTRING=`echo $VERSIONNUM | awk -F "." '{print $1 "." $2 ".'$NEWSUBVERSION'" }'`
-echo $NEWVERSIONSTRING > ./version.txt
-echo $NEWVERSIONSTRING
